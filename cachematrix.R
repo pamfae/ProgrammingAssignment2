ccfb3765babfc1f902c6903b379e4894457ed843
Pamella Fae Jackson <pamfae@gmail.com>
2015-12-27 17:02:03 -0500

    Edit search/replace typos. Added cacheSolve body
    Added the cacheSolve function body, and removed some search
    and replace errors that caused the assignment operator to be reduced
    to the less than.
    
    Also, removed typo from get() function, which changed return
    variable name to mx.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,22 @@
-## makeCacheMatrix and cacheSolve work as a pair to cache the inverse
-## of a given matrix, x. 
+## makeCacheMatrix and cacheSolve work as a pair to cache 
+## the inverse of a given matrix, x. 
+## It returns a list of functions that 
+## set and get the matrix
+## and set and get the inverse of the matrix
 
 ## This function creates a special matrix 
 ## object that can cache its inverse. 
-makeCacheMatrix < function(x = matrix()) {
-  m < NULL
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
   
-  set < function(y) {
+  set <- function(y) {
+    #save matrix to different environment
     x <<- y
     m <<- NULL
   }
-  get < function()mx
-  setInverse < function(m_inverse) m <<- m_inverse
-  getInverse < function() m
+  get <- function()x
+  setInverse <- function(m_inverse) m <<- m_inverse
+  getInverse <- function() m
   
   list(set = set, get = get,
        setInverse = setInverse,
@@ -24,6 +28,16 @@ makeCacheMatrix < function(x = matrix()) {
 ## but only if the matrix's inverse is not already 
 ## cached. It uses the base package's 'solve' function
 ## to play the actual inverse role. 
-cacheSolve < function(x, ...) {
+cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  m_inverse <- x$getInverse()
+  if(!is.null(m_inverse)){
+    message("getting cached data")
+    return(m_inverse)
+  }
+  
+  m_data <- x$get()
+  inverse <- solve(m_data, ...)
+  x$setInverse(m_inverse)
+  m_inverse
 }
